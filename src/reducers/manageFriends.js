56595f395d6431f44a1f05c480404e94d6f99e1f
github username <altmanguitar@gmail.com>
2022-01-16 23:10:37 -0500

    commit tree 3cbd1080e60f68ceec9075ad4b2ec5bf91f24c79

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,26 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return { friends: [...state.friends, action.friend] };
+        case "REMOVE_FRIEND":
+            const exFriendId = state.friends.findIndex(friend => friend.id === action.id)
+            // return { friends: [...state.friends.slice(0, exFriendId), ...state.friends.slice(exFriendId + 1)] };
+            return { friends: state.friends.filter(friend => {return friend.id !== action.id })}
+        default:
+            return state;
+    }
 }
+
+// action = {
+//     type: "ADD_FRIEND",
+//     friend: {
+//       name: "Chrome Boi",
+//       hometown: "NYC",
+//       id: 1,
+//     },
+//   };
+
+//   action = {
+//     type: "REMOVE_FRIEND",
+//     id: 1,
+//   };
